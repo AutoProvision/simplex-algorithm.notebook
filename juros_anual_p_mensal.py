b6a9564337bfdcec3b374f90788432146fc6480d
joaqu1m <joaquim.pires@sptech.school>
2024-10-07 21:59:36 -0300

    atualização de perfil para modalidade

--- juros_anual_p_mensal.py
+++
@@ -1,9 +0,0 @@
-def juros_anual_p_mensal(taxa_anual, periodo_desejado, periodo_atual):
-    a = 1 + (taxa_anual / 100)
-    b = periodo_desejado/periodo_atual
-    c = round(a**b, 4)
-    d = c - 1
-    e = round(d * 100, 2)
-    return e
-
-print(juros_anual_p_mensal(0.8499999999999952, 12, 1))
